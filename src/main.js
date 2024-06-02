db0a0b06b0698e2489774f6b5ecef40cc2a378f8
Екатерина <ironsupergirl@gmail.com>
2024-06-02 12:27:16 +0300

    add js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import './js/header.js';
-import './js/hero.js';
-import './js/about-me.js';
-import './js/tech-together.js';
-import './js/benefits.js';
-import './js/faq.js';
-import './js/projects.js';
 import './js/reviews.js';
 import './js/work-together.js';
-import './js/footer.js';
