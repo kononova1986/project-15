1b41c6b3758c8d48d29399c79ca17f42cb9579ba
Екатерина <ironsupergirl@gmail.com>
2024-06-14 19:59:14 +0300

    update

--- src/js/header.js
+++ src/js/header.js
@@ -4,33 +4,33 @@ const menuBackdrop = document.querySelector('.menu-backdrop');
 const closeModalBtnEl = document.querySelector('.close-modal-btn ');
 
 function toggleModal() {
-    menuBackdrop.classList.toggle('is-open');
+  menuBackdrop.classList.toggle('is-open');
 }
 
 menuBtnEl.addEventListener('click', toggleModal);
 
 closeModalBtnEl.addEventListener('click', toggleModal);
 
-navigationListEl.addEventListener('click', (event) => {
-    if (event.target) {
-        toggleModal();
-    }
-})
+navigationListEl.addEventListener('click', event => {
+  if (event.target) {
+    toggleModal();
+  }
+});
 
 const anchors = document.querySelectorAll('a[class="nav-link"]');
 
 anchors.forEach(anchor => {
-    anchor.addEventListener('click', event => {
-        event.preventDefault();
+  anchor.addEventListener('click', event => {
+    event.preventDefault();
 
-        const sectionsId = anchor.getAttribute('href').substring(1);
+    const sectionsId = anchor.getAttribute('href').substring(1);
 
-        document.getElementById(sectionsId).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
-})
+    document.getElementById(sectionsId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});
 
 const btn = document.querySelector('.button-background');
 const svgBlack = document.querySelector('.icon-btn-black');
@@ -38,37 +38,57 @@ const svgWhite = document.querySelector('.icon-btn');
 const svgNext = document.querySelectorAll('.icon-next, .icon-back');
 const head = document.querySelector('.header');
 const menuMobile = document.querySelector('.menu-backdrop');
+const modalTitle = document.querySelector('.h-modal');
+const modalP = document.querySelector('.p-modal');
 
 btn.addEventListener('click', () => {
-if (svgBlack.style.display === 'none' || svgBlack.style.display === '') {
+  if (svgBlack.style.display === 'none' || svgBlack.style.display === '') {
     svgBlack.style.display = 'block';
     svgWhite.style.display = 'none';
     document.body.style.backgroundColor = '#292929';
     head.style.backgroundColor = '#292929';
+    modalTitle.style.color = '#00b068';
+    modalP.style.color = '#292929';
     menuMobile.style.backgroundColor = '#292929';
-    svgNext.forEach(element => {element.style.stroke = "#FFFFFF";});
-    document.querySelectorAll('input, textarea, .links-section , .swiper-slide, .marquee2').forEach(element => {
-    element.style.background = '#2a2d32';
-});
-document.querySelectorAll('h1, h2, h3, p, li, a').forEach(element => {element.style.color = '#FFFFFF';});
-document.querySelectorAll('.icon-faq-up, .header-icon').forEach(element => {
-    element.style.stroke = '#FFFFFF';
-    element.style.fill = '#FFFFFF';
-});      
-} else {
+    svgNext.forEach(element => {
+      element.style.stroke = '#FFFFFF';
+    });
+    document
+      .querySelectorAll(
+        'input, textarea, .links-section , .swiper-slide, .marquee2'
+      )
+      .forEach(element => {
+        element.style.background = '#2a2d32';
+      });
+    document.querySelectorAll('h1, h2, h3, p, li, a').forEach(element => {
+      element.style.color = '#FFFFFF';
+    });
+    document.querySelectorAll('.icon-faq-up, .header-icon').forEach(element => {
+      element.style.stroke = '#FFFFFF';
+      element.style.fill = '#FFFFFF';
+    });
+  } else {
     svgBlack.style.display = 'none';
     svgWhite.style.display = 'block';
     document.body.style.backgroundColor = '#FFFFFF';
     head.style.backgroundColor = '#FFFFFF';
-     menuMobile.style.backgroundColor = '#FFFFFF';
-    svgNext.forEach(element => {element.style.stroke = "#292929";});
+    menuMobile.style.backgroundColor = '#FFFFFF';
+    svgNext.forEach(element => {
+      element.style.stroke = '#292929';
+    });
     document.querySelectorAll('h1, h2, h3, p, li, a').forEach(element => {
-    element.style.color = '#292929';
+      element.style.color = '#292929';
+    });
+    document
+      .querySelectorAll(
+        'input, textarea, .links-section , .swiper-slide, .marquee2'
+      )
+      .forEach(element => {
+        element.style.background = 'var(--light-grey)';
+      });
+    document.querySelectorAll('.icon-faq-up, .header-icon').forEach(element => {
+      element.style.stroke = '#292929';
+      element.style.fill = '#292929';
+    });
+  }
 });
-    document.querySelectorAll('input, textarea, .links-section , .swiper-slide, .marquee2').forEach(element => {
-    element.style.background = 'var(--light-grey)';}); 
-document.querySelectorAll('.icon-faq-up, .header-icon').forEach(element => {
-    element.style.stroke = '#292929';
-    element.style.fill = '#292929';
-});        
-}});
